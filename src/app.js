f6549282348678cc0f93009d857bb54a837d2f3a
DevYr <dev.yrneh@gmail.com>
2019-06-14 11:16:05 -0700

    Added Vuex.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import VueResource from 'vue-resource';
+import Vuex from 'vuex';
 
 import App from './App.vue';
 import router from './router';
 
 Vue.use(VueResource);
+Vue.use(Vuex);
 
 new Vue({
   el: '#app',
